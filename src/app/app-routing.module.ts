e4259fb37c0396570783446b5c4aaafc6ffd7899
andres.mera <feli041089@gmail.com>
2024-02-19 14:40:04 -0500

    dev : Resolucion de la tarea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomePagesComponent } from './shared/pages/home-pages/home-pages.component';
 import { AboutPagesComponent } from './shared/pages/about-pages/about-pages.component';
+import { ContactPagesComponent } from './shared/pages/contact-pages/contact-pages.component';
 
 const routes: Routes = [
   { path: '', component: HomePagesComponent },
   { path: 'about', component: AboutPagesComponent },
+  { path: 'contact', component: ContactPagesComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,12 +5,14 @@ import { RouterModule } from '@angular/router';
 import { HomePagesComponent } from './pages/home-pages/home-pages.component';
 import { AboutPagesComponent } from './pages/about-pages/about-pages.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ContactPagesComponent } from './pages/contact-pages/contact-pages.component';
 
 
 
 @NgModule({
   declarations: [
     AboutPagesComponent,
+    ContactPagesComponent,
     HomePagesComponent,
     SidebarComponent,
   ],
@@ -20,6 +22,7 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
   ],
   exports: [
     AboutPagesComponent,
+    ContactPagesComponent,
     HomePagesComponent,
     SidebarComponent,
   ]
