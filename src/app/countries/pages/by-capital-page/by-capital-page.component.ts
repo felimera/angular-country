a22a2ce91afaccb0cb4e11244200544dded204df
andres.mera <feli041089@gmail.com>
2024-02-20 09:06:51 -0500

    dev : Mostrar los paises en pantalla.

--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-by-capital-page',
@@ -7,7 +9,20 @@ import { Component } from '@angular/core';
 })
 export class ByCapitalPageComponent {
 
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) { }
+
   searchByCapital(term: string): void {
-    console.log({ term })
+    this.countriesService
+      .searchCapital(term)
+      .subscribe({
+        next: (resp: Country[]) => {
+          if (resp) {
+            this.countries = resp;
+          }
+        },
+        error: (resp) => console.log(resp.error)
+      });
   }
 }
