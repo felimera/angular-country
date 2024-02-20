3f3958925e1575997de97b2f0ba6e72fd7fdfa50
andres.mera <feli041089@gmail.com>
2024-02-20 13:17:37 -0500

    dev : Mostrar informacion del pais.

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
 import { switchMap } from 'rxjs';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'app-country-page',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs';
   styles: ``
 })
 export class CountryPageComponent implements OnInit {
+
+  public country?: Country;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -23,11 +26,8 @@ export class CountryPageComponent implements OnInit {
       )
       .subscribe({
         next: (country) => {
-          if (!country)
-            return this.router.navigateByUrl('');
-
-          console.log({ country });
-          return;
+          if (!country) return this.router.navigateByUrl('');
+          return this.country = country;
         }
       });
   }
