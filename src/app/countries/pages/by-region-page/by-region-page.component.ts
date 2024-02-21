a0d49b32435048aa84f47fffcefe458bd1476923
andres.mera <feli041089@gmail.com>
2024-02-21 07:08:46 -0500

    dev : Buscar por region - Optimizacion

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'America' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'app-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,14 +11,17 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
 
-
   public countries: Country[] = [];
+  public regions: Region[] = ['Africa', 'America', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) { }
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
+    this.selectedRegion = region;
+
     this.countriesService
-      .searchRegion(term)
+      .searchRegion(region)
       .subscribe({
         next: (resp: Country[]) => {
           if (resp) {
